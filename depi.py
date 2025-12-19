39e858c5bd73ea1d932a119b8922d946d53120c5
yoclutchedits <pitambers98@gmail.com>
2025-12-19 19:10:27 +0545

    Update depi.py

--- depi.py
+++ depi.py
@@ -1,20 +1,24 @@
 import tensorflow as tf
 from tensorflow.keras import layers, models
 import matplotlib.pyplot as plt
-x_train, y_train = tf.keras.datasets.mnist.load_data()[0]
-x_train,x_test = x_train / 255.0, x_test / 255.0
-model=models.Sequential([
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+x_train = x_train / 255.0
+x_test = x_test / 255.0
+model = models.Sequential([
     layers.Flatten(input_shape=(28, 28)),
     layers.Dense(128, activation='relu'),
     layers.Dense(10, activation='softmax')
 ])
-model.compile(optimizer='adam',
-                loss='sparse_categorical_crossentropy',
-                metrics=['accuracy'])
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
 model.fit(x_train, y_train, epochs=5)
-test_loss, test_accucracy = model.evaluate(x_test, y_test)
+test_loss, test_accuracy = model.evaluate(x_test, y_test)
 print(f'Test accuracy: {test_accuracy}')
-pridctions = model.predict(x_test)
-plt.iamshow(x_test[0], cmap=plt.cm.binary)
-plt.title(f'Predicted :{pridctions[0].argmax()}')
+predictions = model.predict(x_test)
+plt.imshow(x_test[0], cmap=plt.cm.binary)
+plt.title(f'Predicted: {predictions[0].argmax()}')
+plt.axis('off')
 plt.show()
